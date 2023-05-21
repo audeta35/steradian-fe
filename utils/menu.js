b24e31d3a3b2b671496d67bdd75a5e303310186f
audeta35 <audeta35@gmail.com>
2023-05-22 06:58:16 +0700

    #patch

---
+++ utils/menu.js
@@ -0,0 +1,35 @@
+export const menuDashboard = [
+  {
+    title: "user",
+    href: "/user",
+    desc: "add/edit/delete user",
+  },
+  {
+    title: "admin",
+    href: "/admin",
+    desc: "add/edit/delete admin",
+  },
+  {
+    title: "order",
+    href: "/order",
+    desc: "add/edit/delete order",
+  },
+  {
+    title: "cars",
+    href: "/cars",
+    desc: "add/edit/delete cars",
+  },
+];
+
+export const menuAuth = [
+  {
+    title: "login user",
+    href: "/login-user",
+    desc: "login as user",
+  },
+  {
+    title: "login admin",
+    href: "/login-admin",
+    desc: "login as admin",
+  },
+];
