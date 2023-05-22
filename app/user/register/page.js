40e4b3e50691a6e5e413b1941b9df9d3727f27e8
audeta35 <audeta35@gmail.com>
2023-05-22 17:30:48 +0700

    #patch: register page

--- app/user/register/page.js
+++ app/user/register/page.js
@@ -32,8 +32,13 @@ export default function Page() {
         </div>
       </div>
       <div className="md:w-1/3 max-w-sm">
-        <input onChange={(e) => onChange(e, 'email')} className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded" type="text" placeholder="Email Address" />
+        <input onChange={(e) => onChange(e, 'username')} className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="text" placeholder="Username" />
+        <input onChange={(e) => onChange(e, 'email')} className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="text" placeholder="Email" />
         <input onChange={(e) => onChange(e, 'password')} className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="password" placeholder="Password" />
+        <input onChange={(e) => onChange(e, 'phoneNumber')} className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="text" placeholder="Phone Number" />
+        <input onChange={(e) => onChange(e, 'city')} className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="text" placeholder="City" />
+        <input onChange={(e) => onChange(e, 'zip')} className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="text" placeholder="ZIP" />
+        <input onChange={(e) => onChange(e, 'address')} className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="text" placeholder="Address" />
 
         <div className="text-center md:text-left">
           <button onClick={() => onSubmit()} className="mt-4 bg-blue-400 hover:bg-blue-500 px-4 py-2 text-white uppercase rounded text-xs tracking-wider w-full" type="submit">Register</button>
